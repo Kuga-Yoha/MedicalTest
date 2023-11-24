5f39c881511946c28fe811fa4049cce6e517f6bc
Patel-Prem <patelprem12320@gmail.com>
2023-11-23 23:54:27 -0500

    added dependencies and created Entities

---
+++ app/src/main/java/com/group6/medicaltest/Test.kt
@@ -0,0 +1,15 @@
+package com.group6.medicaltest
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName="test")
+data class Test(
+    @PrimaryKey(autoGenerate = true)
+    val testId: Long,
+    val patientId: String,
+    val nurseId: Long,
+    val BPL: String,
+    val BPH: String,
+    val temperature: String,
+)
