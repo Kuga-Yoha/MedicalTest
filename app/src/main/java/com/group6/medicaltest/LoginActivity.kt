904c4194fc7615f6471ed251cd875098bcddb0b3
Kugavathanan Yohanathan <kugajpx2012@gmail.com>
2023-11-25 10:46:03 -0500

    modified login activity

--- app/src/main/java/com/group6/medicaltest/LoginActivity.kt
+++ app/src/main/java/com/group6/medicaltest/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.group6.medicaltest
 
+import android.app.Application
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,14 +8,17 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.room.Room
 import com.group6.medicaltest.entity.Nurse
 import com.group6.medicaltest.repository.NurseRepository
+import com.group6.medicaltest.viewmodel.LoginViewModel
+import androidx.lifecycle.ViewModelProvider
 
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity (): AppCompatActivity() {
     //private lateinit var nurseId: EditText
-    private var nurseRepo: NurseRepository = NurseRepository(application)
+    private lateinit var viewModel: LoginViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +28,13 @@ class LoginActivity : AppCompatActivity() {
         val password = findViewById<EditText>(R.id.editTextTextPassword)
         val sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
 
-        val login = findViewById<View>(R.id.buttonLogin) as Button
-        login.setOnClickListener() {
-            //Login for Validating Credentials with Database
-            val nurse = nurseRepo.getNurseByIdAndPassword(Nurse(nurseId.text.toString().toLong(), "", "", "", password.text.toString()))
-            if(nurse != null){
+        // Initialize the ViewModel
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+        // Observe the login result
+        viewModel.loginResult.observe(this, Observer { loginSuccessful ->
+            if (loginSuccessful) {
+                // Handle successful login
 
                 // Storing the username (nurseId) into Shared Preferences.
                 val editor = sharedPreferences.edit()
@@ -38,8 +44,16 @@ class LoginActivity : AppCompatActivity() {
                 intent = Intent(applicationContext, SelectOperationActivity::class.java)
                 startActivity(intent)
             } else {
+                // Handle unsuccessful login
                 Toast.makeText(this, "UserName/Password Wrong, Check Again", Toast.LENGTH_LONG).show()
             }
+        })
+
+        val login = findViewById<View>(R.id.buttonLogin) as Button
+        login.setOnClickListener() {
+            // Trigger the login operation in the ViewModel
+            viewModel.login(nurseId.text.toString().toInt(), password.text.toString())
+
         }
 
         // Fetching the value from Shared Preferences.
