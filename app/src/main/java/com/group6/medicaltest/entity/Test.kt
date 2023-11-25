b505c3844d631190a5ac7a936b82731ac59efafa
Kugavathanan Yohanathan <kugajpx2012@gmail.com>
2023-11-25 10:43:43 -0500

    data type modified

--- app/src/main/java/com/group6/medicaltest/entity/Test.kt
+++ app/src/main/java/com/group6/medicaltest/entity/Test.kt
@@ -23,9 +23,9 @@ import androidx.room.PrimaryKey
 )
 data class Test(
     @PrimaryKey(autoGenerate = true)
-    val testId: Long,
-    val patientId: String,
-    val nurseId: Long,
+    val testId: Int,
+    val patientId: Int,
+    val nurseId: Int,
     val BPL: String,
     val BPH: String,
     val temperature: String,
